a82d41d6d409467078e00fae1168eceef43d46b7
Clara8c <162982904+Clara8c@users.noreply.github.com>
2024-03-11 13:02:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,24 @@
 import streamlit as st
 name = st.text_input("Your name")
 st.write ("Hello" + name)
+import streamlit as st
+
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
+
+if genre == ':rainbow[Comedy]':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn\'t select comedy.")
+  
+  
+input ="lapin"
+list_possibilities=["rabbit","ratatouille","pig","rat"]
+correct_answer="rabbit"
+st.write("Traduis" + input)
+for i in range (len(list_possibilities)):
+  st.button(list_possibilities[i])
+
+  
