14702b05af461f5e6f7a343f51411bb8b4ad85b7
Clara8c <162982904+Clara8c@users.noreply.github.com>
2024-03-18 12:01:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTaUESVFbUnxC9q
 st.dataframe(voc)
 l=voc.shape(voc)
 i=hp.random.choice(range(l))
-word_fr=voc['Definition'].values
+word_fr=voc['Definition'].values[i]
 word_chi=voc['Pinyin'].values[i]
 st.write (word_fr+" "+word_chi
