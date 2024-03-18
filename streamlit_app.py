a08328456b3425bf5abd0eb4ad8243b170860c95
Clara8c <162982904+Clara8c@users.noreply.github.com>
2024-03-18 11:35:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,8 @@ if genre == ':rainbow[Comedy]':
     st.write('You selected comedy.')
 else:
     st.write("You didn\'t select comedy.")
+
+import streamlit as st
+import pandas as pd
+voc=pd.read_csv('https://docs.google.com/spreadsheets/d/1jH6kJJISFA0Ye4gE4CzG7ZEjBJhCRelGOZN-HnxyZBQ/edit?usp=sharing')
+st.dataframe(voc)
